daec0184d2226089b18454109d32033bf20e0f49
tyaG10 <titmambyves6@gmail.com>
2024-09-11 19:27:05 +0200

    Update ForgotPasswordPage.jsx

--- frontend/src/pages/ForgotPasswordPage.jsx
+++ frontend/src/pages/ForgotPasswordPage.jsx
@@ -2,7 +2,7 @@ import { motion } from 'framer-motion';
 import { useState } from 'react';
 import { useAuthStore } from '../store/authStore';
 import { Link } from 'react-router-dom';
-import Input from '../components/input';
+import Input from '../components/Input';
 import { ArrowLeft, Loader, Mail } from 'lucide-react';
 
 const ForgotPasswordPage = () => {
